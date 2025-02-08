159d107651d1d57a0a29930c92740555338a1672
Mihael <mihael..cindri9@gmail.com>
2025-02-08 18:42:39 +0100

    Guest guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,24 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
 import {SearchComponent} from './flights/search/search.component';
-import {ResultsComponent} from './flights/results/results.component';
+import {TravelDetailComponent} from './flights/travel-detail/travel-detail.component';
 import {DashboardComponent} from './admin/dashboard/dashboard.component';
 import {NgModule} from '@angular/core';
+import {AdminGuard} from './guards/admin.guard';
+import {ProfileComponent} from './flights/profile/profile.component';
+import {SavedTravelsComponent} from './flights/saved-travels/saved-travels.component';
+import {GuestGuard} from './guards/guest.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/flights/search', pathMatch: 'full' },
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
   { path: 'flights/search', component: SearchComponent },
-  { path: 'flights/results', component: ResultsComponent },
-  { path: 'admin/dashboard', component: DashboardComponent },
+  { path: 'flights/travel-detail', component: TravelDetailComponent },
+  { path: 'flights/profile', component: ProfileComponent, canActivate: [GuestGuard] },
+  { path: 'flights/saved-travels', component: SavedTravelsComponent, canActivate: [GuestGuard] },
+  { path: 'admin/dashboard', component: DashboardComponent, canActivate: [AdminGuard] },
+  { path: '**', redirectTo: 'flights/search' }
 ];
 
 @NgModule({
